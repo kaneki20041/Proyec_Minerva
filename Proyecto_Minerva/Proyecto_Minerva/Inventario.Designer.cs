bfb9270831bfd7c3980fc2f62b7e58053dd4e9c4
Cristian More <cristianmore4572@gmail.com>
2024-10-09 12:57:54 -0500

    Interfaz Inventario v0.5

---
+++ Proyecto_Minerva/Proyecto_Minerva/Inventario.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace CapaPresentacion
+{
+    partial class Inventario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            textBox1 = new TextBox();
+            button5 = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = Color.MintCream;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(28, 217);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(629, 330);
+            dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(44, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(79, 17);
+            label1.TabIndex = 1;
+            label1.Text = "Buscar por:";
+            label1.Click += label1_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Nombre", "Categoria", "Talla", "Color", "Stock" });
+            comboBox1.Location = new Point(20, 46);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 25);
+            comboBox1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(groupBox2);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            groupBox1.Location = new Point(668, 217);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(161, 289);
+            groupBox1.TabIndex = 3;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Buscar";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button1);
+            groupBox2.Controls.Add(button5);
+            groupBox2.Controls.Add(textBox1);
+            groupBox2.Location = new Point(12, 90);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(134, 176);
+            groupBox2.TabIndex = 3;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Ingrese dato";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(8, 43);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(120, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.LightBlue;
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button5.Location = new Point(8, 84);
+            button5.Name = "button5";
+            button5.Size = new Size(120, 32);
+            button5.TabIndex = 50;
+            button5.Text = "Filtrar";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LightBlue;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(8, 134);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 32);
+            button1.TabIndex = 51;
+            button1.Text = "Eliminar filtro";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.LightBlue;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(688, 515);
+            button2.Name = "button2";
+            button2.Size = new Size(120, 32);
+            button2.TabIndex = 51;
+            button2.Text = "Borrar Prenda";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // Inventario
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.MintCream;
+            ClientSize = new Size(851, 573);
+            Controls.Add(button2);
+            Controls.Add(groupBox1);
+            Controls.Add(dataGridView1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Inventario";
+            Text = "Inventario";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private ComboBox comboBox1;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private TextBox textBox1;
+        private Button button5;
+        private Button button1;
+        private Button button2;
+    }
+}
